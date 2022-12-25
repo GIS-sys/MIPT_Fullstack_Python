2912c9cddfab30b8455d2df685f2474feebb64ea
EgorovGA <egorov.ga@minc.tech>
2022-12-26 01:31:56 +0300

    Port is fixed

--- hookies/src/index.js
+++ hookies/src/index.js
@@ -5,9 +5,12 @@ import "./components/styles/style.css";
 
 // http://158.160.14.47:8000
 // http://127.0.0.1:8000
-// console.log(process.env.REACT_APP_BACKEND_URL);
-if (!process.env.REACT_APP_BACKEND_URL) global.BACKEND_URL = "http://127.0.0.1:8000";
-
+console.log(process.env.REACT_APP_BACKEND_URL);
+if (process.env.REACT_APP_BACKEND_URL) {
+    global.BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+} else {
+    global.BACKEND_URL = "http://127.0.0.1:8000";
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
